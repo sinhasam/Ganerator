021d486171ddafe43605073daa9dddc718a2d428
Robert Fairley <robertthomasfairley@gmail.com>
2017-01-21 03:38:20 -0800

    update for test

--- website/hello.lua
+++ website/hello.lua
@@ -1,2 +1,19 @@
-local lightSeconds = distanceToMoon / speedOfLight
-log('It takes '..lightSeconds..' seconds for light to travel from the Moon.')
+local colors = { 'red', 'blue', 'green', 'orange', 'yellow', 'purple' }
+local balls = {}
+
+
+for _, color in pairs(colors) do
+	local radius = math.random(5, 25)
+	local circle = createCircle(color, radius)
+
+	local velX = math.random(-8, 8)
+	local velY = math.random(-8, 8)
+
+	circle.onUpdate = function ()
+		circle.x = circle.x + velX
+		circle.y = circle.y + velY
+
+		if circle.x < radius or circle.x > 300 - radius then velX = -velX end
+		if circle.y < radius or circle.y > 300 - radius then velY = -velY end
+	end
+end
